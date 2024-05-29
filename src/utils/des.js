a9bdda889d4afa9913846f69c949e32e1edd6e40
Mustafa <mustafamengutayy@gmail.com>
2024-05-29 16:32:09 +0300

    refactor: update decrypt's fields

--- src/utils/des.js
+++ src/utils/des.js
@@ -1,5 +1,14 @@
 import CryptoJS from "crypto-js";
 
+const SECRET_KEY = 'cmpetest!';
+
+const fields = [
+    'socialInsurance',
+    'generalHealthSystem',
+    'incomeTax',
+    'totalTaxAmount',
+];
+
 export const decrypt = (taxes) => {
     fields.forEach((field) => {
         taxes[field] = CryptoJS.DES.decrypt(taxes[field], SECRET_KEY).toString(
